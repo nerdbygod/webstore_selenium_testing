bcacae8e614a28a75f452ba92736f9e7f3fb9adb
nerdbygod <pavelartemov53@gmail.com>
2020-09-28 00:43:43 +0400

    product page test and locators added + base page modified

--- pages/locators.py
+++ pages/locators.py
@@ -14,3 +14,11 @@ class LoginPageLocators():
     LOGIN_EMAIL = (By.CSS_SELECTOR, "#id_login-username_invalid")
     LOGIN_PASSWORD = (By.CSS_SELECTOR, "#id_login-password_invalid")
     LOGIN_SUBMIT_BUTTON = (By.CSS_SELECTOR, "button[name='login_submit']_invalid")
+
+
+class ProductPageLocators():
+    PROMO_ITEM_ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form button")
+    PROMO_ITEM_TITLE = (By.CSS_SELECTOR, ".product_main > h1")
+    PROMO_ITEM_PRICE = (By.CSS_SELECTOR, ".product_main > .price_color")
+    ITEM_ADDED_MESSAGE = (By.CSS_SELECTOR, "#messages > div:nth-child(1)")
+    BASKET_PRICE_NOTIFICATION = By.CSS_SELECTOR, "#messages > div:last-child .alertinner strong"
---
+++ pages/product_page.py
@@ -0,0 +1,24 @@
+from .main_page import MainPage
+from .locators import ProductPageLocators
+
+
+class ProductPage(MainPage):
+    def add_to_cart(self):
+        add_to_cart_button = self.browser.find_element(*ProductPageLocators.PROMO_ITEM_ADD_TO_CART_BUTTON)
+        add_to_cart_button.click()
+
+    def item_price_text(self):
+        item_price = self.browser.find_element(*ProductPageLocators.PROMO_ITEM_PRICE)
+        return item_price.text
+
+    def item_title_text(self):
+        item_title = self.browser.find_element(*ProductPageLocators.PROMO_ITEM_TITLE)
+        return item_title.text
+
+    def should_be_item_in_basket(self, item_title_text, item_price_text):
+        item_in_basket_title_notif = self.browser.find_element(*ProductPageLocators.ITEM_ADDED_MESSAGE)
+        item_in_basket_price_notif = self.browser.find_element(*ProductPageLocators.BASKET_PRICE_NOTIFICATION)
+        assert item_title_text in item_in_basket_title_notif.text, \
+            "Item title should be correctly displayed in the basket"
+        assert item_price_text in item_in_basket_price_notif.text, \
+            "Item price should be correctly displayed in the basket"
